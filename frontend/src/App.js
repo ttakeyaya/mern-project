631e2ea1b5e03e8729515aed8da6452ef49bb50e
ttakeyaya <taketakemoji@gmail.com>
2022-09-03 13:30:59 +0900

    Add Header comp and Home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header';
+
+import Home from './pages/Home';
 
 function App() {
-  return <div>Hello My App</div>;
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FaTicketAlt } from 'react-icons/fa';
+
+function Header() {
+  return (
+    <header className="header">
+      <ul>
+        <li>Sign in</li>
+        <li>Register</li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
