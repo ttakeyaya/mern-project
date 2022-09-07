c7c2d00c5d6e1d3cc38ed9d673b9a1cb52e61561
ttakeyaya <taketakemoji@gmail.com>
2022-09-07 20:41:36 +0900

    デプロイ用にコード修正

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./DB/mongoDB');
+const path = require('path');
 const app = express();
 const { errorHandler } = require('./middleware/errorMiddleware');
 
@@ -14,10 +15,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/cards', require('./routes/cardRoutes'));
 
+// Reactアプリ
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../../frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(__dirname, '../../', 'frontend', 'build', 'index.html')
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Flash Card App' });
+  });
+}
+
 // middleware for handling error
 app.use(errorHandler);
-// app.get('/', (req, res) => {
-//   res.send('launch the app');
-// });
 
 module.exports = app;
